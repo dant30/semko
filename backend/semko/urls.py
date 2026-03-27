16a66c8862d7070eff3d35968e6c556f9057ba19
Dant30 <dankarish.dk@gmail.com>
2026-03-27 09:05:01 +0300

    health check fixed

--- backend/semko/urls.py
+++ backend/semko/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from apps.core.views.api import HealthCheckAPIView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/health/', HealthCheckAPIView.as_view(), name='health'),
