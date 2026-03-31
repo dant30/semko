402af4cd9b2eccdabad3bff4fc15c4b8a87da128
Dant30 <dankarish.dk@gmail.com>
2026-03-31 09:13:42 +0300

    enhancing styling and tailwind

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const appLoader = document.getElementById("app-loader");
+if (appLoader) {
+  appLoader.classList.add("hidden");
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
